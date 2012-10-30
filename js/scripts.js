66fe10862f5040e4d1fe8ce134be7319576bb689
Cristina Solana <cs@nightshiftcreative.com>
2012-10-30 15:58:06 -0400

    form, js shell

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function compute() {
+  var serial = document.getElementById('serial');
+  var modSeven = document.getElementById('mod7');
+  // get some computing up in here...
+  return false;
+}
+
+function init() {
+
+}
+
+window.onload = init;
