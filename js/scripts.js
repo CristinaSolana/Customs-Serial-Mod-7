dc09b04cb5a72a3906dc9ee2b83557ad2a467b48
Cristina Solana <cs@nightshiftcreative.com>
2012-11-11 15:57:27 -0500

    calculator js, style and html tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
+// Calulation instructions
+// Divide assigned 8-digit serial by 7
+// Multiply remainder (first digit after period unrounded) by 7
+// Round up to next whole number
+// Add digit to end of serial
+
 function compute() {
+  'use strict';
   var serial = document.getElementById('serial');
-  var modSeven = document.getElementById('mod7');
-  // get some computing up in here...
+  var serialMod;
+  var modSeven;
+  if (serial.value.length == 8) {
+    serialMod = serial.value%7;
+      modSeven = serial.value + serialMod;
+  } else {
+    serial.value = 'Sorry, I need a serial with 8 digits';
+  }
+  document.getElementById('check-digit').value = modSeven;
   return false;
 }
-
 function init() {
-
+  'use strict';
+  document.getElementById('customs-mod7').onsubmit = compute;
 }
-
 window.onload = init;
